b0ad2c85b2a2b4d7f4c88b6295264460b7bf5f04
Ankit <108567827+thatboyankit@users.noreply.github.com>
2023-04-15 11:57:59 +0530

    Update helper-hardhat.config.js

--- helper-hardhat.config.js
+++ helper-hardhat.config.js
@@ -4,14 +4,14 @@ const networkConfig = {
     11155111: {
         name: "Sapoila",
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
-        entryFee: ethers.util.parseEthers("0.3"),
+        entryFee: ethers.utils.parseEther("0.3"),
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
         subscriptionId: "something",
         gasLimit: "2,500,000"
     },
     31337: {
         name: "localhost",
-        entryFee: ethers.util.parseEthers("3"),
+        entryFee: ethers.utils.parseEther("3"),
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
         gasLimit: "2,500,000"
     }
